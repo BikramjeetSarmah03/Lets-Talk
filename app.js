a0dc62343d7c05a055e3a7ba0ede9c09bc823374
Bikramjeet Sarmah <9854bikram@gmail.com>
2023-02-09 03:07:41 +0530

    Limit increased

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const middleware = require("./utils/middleware");
 const app = express();
 
 app.use(cors());
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 // routes
 const authRoutes = require("./routes/authRoutes");
