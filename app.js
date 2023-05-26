8efb3c33a779315c8afcf75bbb90deda508151bf
Bikramjeet Sarmah <83879699+BikramjeetSarmah03@users.noreply.github.com>
2023-05-26 11:59:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const middleware = require("./utils/middleware");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin:"https://project-lets-talk.netlify.app",
+ ));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
